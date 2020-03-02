167f6603a32847d7b0e319dea2d1cad9444c97c3
downtomarsmama <54377186+downtomarsmama@users.noreply.github.com>
2020-03-02 10:15:44 -0500

    Add files via upload

--- Helloworld.js
+++ Helloworld.js
@@ -52,4 +52,9 @@ while(i < cohortOneStudents.length){
    
   }
 
+  var harryPotterTitles = ["Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of Fire", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Half-Blood Prince", "Harry Potter and the Deathly Hallows"];
+  for(var i = 0; i < harryPotterTitles.length; i++){
+    console.log(harryPotterTitles[i]);
+  }
+
   
