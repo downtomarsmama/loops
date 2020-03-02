c329e8e2e68d12e172cf58b9d981ee365498122a
downtomarsmama <54377186+downtomarsmama@users.noreply.github.com>
2020-03-02 10:03:52 -0500

    Add files via upload

---
+++ Helloworld.js
@@ -0,0 +1,55 @@
+// Loops
+
+
+var i = 1;
+
+while(i < 20){
+  console.log("Hello, world");
+  i++;
+}
+
+var moviesArray = ["Casablanca", "Star Wars", "Singing in the Rain", "The Wizard of Oz", "Die Hard"];
+
+var i = 0; 
+
+while(i < moviesArray.length){
+  console.log(moviesArray[i]);
+  i++;
+}
+
+var i =0;
+
+while(i<100){
+    console.log("TWEET TWEET TWEET")
+    i++;
+}
+
+var favoriteFoods = ["Tacos","Japanese","Pizza"]
+
+var i =0;
+while(i<moviesArray.length){
+    console.log(moviesArray[i]);
+    i++;
+}
+
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+while(i < cohortOneStudents.length){
+    console.log(cohortOneStudents[i]);
+    i++;
+  }
+
+  var interestArray =["hiking","sushi","anime","reading","painting"];
+
+  for(var i = 0; i < interestArray.length; i++)
+  
+  if(interestArray[i]==="reading"){
+    console.log(`One of my absolute favorite is: ${interestArray[i]}`);
+
+  }
+  else{
+    console.log(`One of my interest is: ${interestArray[i]}`);
+   
+  }
+
+  
